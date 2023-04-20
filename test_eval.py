35ca8b15068391715c74b6099e529d9777527e22
maxmanmius <103955904+maxmanmius@users.noreply.github.com>
2023-04-20 09:21:34 -0600

    Updated test_eval.py

--- test_eval.py
+++ test_eval.py
@@ -1,9 +1,24 @@
 import state
 
 state = state.State()
-state.occupied = [[False,False,False,False],
-                  [False,False,False,False],
-                  [False,True,False,False],
-                  [False,False,False,False]]
-assert(state.eval() == 2) # because top 2 lines are empty
-print("PASS")
+state.occupied = [[False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,False],
+                  [False,False,False,False,False,False,False,False,False,True],
+                  [False,False,False,False,False,False,False,False,False,True],
+                  [False,False,False,False,False,False,False,False,False,True],
+                  [False,True,True,True,True,False,False,False,False,True]]
+state.display()
