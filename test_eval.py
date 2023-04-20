2c82d7ce376e07ff89250bff7a6af25058f99c3e
maxmanmius <103955904+maxmanmius@users.noreply.github.com>
2023-04-20 10:29:24 -0600

    Reset test_eval.py

--- test_eval.py
+++ test_eval.py
@@ -1,24 +1,9 @@
 import state
 
 state = state.State()
-state.occupied = [[False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,False],
-                  [False,False,False,False,False,False,False,False,False,True],
-                  [False,False,False,False,False,False,False,False,False,True],
-                  [False,False,False,False,False,False,False,False,False,True],
-                  [False,True,True,True,True,False,False,False,False,True]]
-state.display()
+state.occupied = [[False,False,False,False],
+                  [False,False,False,False],
+                  [False,True,False,False],
+                  [False,False,False,False]]
+assert(state.eval() == 2) # because top 2 lines are empty
+print("PASS")
